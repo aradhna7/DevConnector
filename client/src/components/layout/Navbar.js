5267e2b29dc034654f37a9b6e9aa2c5b59417144
aradhna7 <aradhna.7sharma@gmail.com>
2020-08-29 17:29:37 +0530

    posts and comments

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -10,6 +10,7 @@ const Navbar = ({logout, auth:{ isAuthenticated, loading}}) => {
   const authLinks =(
     <ul>
       <li><Link to="/profiles">Developers</Link></li>
+      <li><Link to="/posts">Posts</Link></li>
       <li><Link to="/dashboard"><i className="fas fa-user"></i> <span class="hide-sm">Dashboard</span></Link></li>
       <li><a href="#!" onClick={logout}><i className="fas fa-sign-out-alt"></i> <span class="hide-sm">Logout</span></a></li>
   </ul>
